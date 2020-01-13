718947edc6e0151e3ea11ea277b2e005f9ec5a9f
Erivks <mordecai.es27@gmail.com>
2020-01-13 13:38:12 -0300

    Add pasta para outras views

---
+++ resources/views/outras/departamentos.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.principal')
+
+@section('titulo', 'Departamentos')
+    
+@section('conteudo')
+    <ul>
+        <li>Computadores</li>
+        <li>Eletronicos</li>
+        <li>Acessórios</li>
+        <li>Roupas</li>
+    </ul>
+@endsection
---
+++ resources/views/outras/produtos.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.principal')
+
+@section('titulo', 'Produtos')
+
+@section('conteudo')
+    <ul>
+        <li>PC</li>
+        <li>Cabo</li>
+        <li>Camisa</li>
+        <li>Chinelo</li>
+    </ul>
+@endsection
